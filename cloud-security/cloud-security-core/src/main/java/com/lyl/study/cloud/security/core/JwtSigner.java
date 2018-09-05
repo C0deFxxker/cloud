c2400fa39667292ff78a8de697bcc4b80260ec7e
C0deFxxker <502291755@qq.com>
2018-09-05 08:43:52 +0800

    完成JwtSigner

--- cloud-security/cloud-security-core/src/main/java/com/lyl/study/cloud/security/core/JwtSigner.java
+++ cloud-security/cloud-security-core/src/main/java/com/lyl/study/cloud/security/core/JwtSigner.java
@@ -1,34 +1,74 @@
 package com.lyl.study.cloud.security.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
+@Data
+@ToString
+@Accessors(chain = true)
 public class JwtSigner {
-    static final String SECRET = "ThisIsASecret";
+    // jwt secret
+    private String secret = "ThisIsASecret";
+    // Token expire in 2 hours.
+    private long expire = 7200000L;
+    // json serializer
+    private ObjectMapper objectMapper;
 
-    public static String generateToken(String username) {
-        Map<String, Object> map = new HashMap<>();
-        //you can put any data in the map
-        map.put("username", username);
-        String jwt = Jwts.builder()
-                .setClaims(map)
-                .setExpiration(new Date(System.currentTimeMillis() + 3600_000_000L))// 1000 hour
-                .signWith(SignatureAlgorithm.HS512, SECRET)
-                .compact();
-        return "Bearer "+jwt; //jwt前面一般都会加Bearer
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> serializeObject(Object object) {
+        try {
+            String json = objectMapper.writeValueAsString(object);
+            return objectMapper.readValue(json, Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void validateToken(String token) {
+    @SuppressWarnings("unchecked")
+    private Object deserializeObject(String clsName, Map<String, Object> map) {
+        try {
+            String json = objectMapper.writeValueAsString(map);
+            Class<?> aClass = Class.forName(clsName);
+            return objectMapper.readValue(json, aClass);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String serializeToken(Object object) {
+        Map<String, Object> claims = serializeObject(object);
+        return Jwts.builder()
+                .setHeaderParam("cls", claims.getClass())
+                .setClaims(claims)
+                .setExpiration(new Date(System.currentTimeMillis() + expire))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public Object deserializeToken(String token) {
         try {
             // parse the token.
-            Map<String, Object> body = Jwts.parser()
-                    .setSigningKey(SECRET)
-                    .parseClaimsJws(token.replace("Bearer ",""))
-                    .getBody();
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token);
+            String cls = (String)claimsJws.getHeader().get("cls");
+            Map<String, Object> body = claimsJws.getBody();
+
+            // deserialize object to cls instance
+            if (cls != null) {
+                return deserializeObject(cls, body);
+            } else {
+                return body;
+            }
         }catch (Exception e){
             throw new IllegalStateException("Invalid Token. "+e.getMessage());
         }
