30ac6058f5518845fa8d784402f6fc41a33bfe63
C0deFxxker <502291755@qq.com>
2018-09-14 18:10:42 +0800

    修改了IllegalOperationException的构建函数

--- cloud-base/src/main/java/com/lyl/study/cloud/base/exception/IllegalOperationException.java
+++ cloud-base/src/main/java/com/lyl/study/cloud/base/exception/IllegalOperationException.java
@@ -1,7 +1,13 @@
 package com.lyl.study.cloud.base.exception;
 
+import org.springframework.boot.logging.LogLevel;
+
 public class IllegalOperationException extends BaseException {
     public IllegalOperationException(int code, String msg) {
         super(code, msg);
+    }
+
+    public IllegalOperationException(int code, String msg, LogLevel logLevel) {
+        super(code, msg, logLevel);
     }
 }
